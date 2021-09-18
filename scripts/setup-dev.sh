c0c728c7b827cb10f39a574263d83326f731c21b
Neil Wang <wangweili457@gmail.com>
2021-09-18 13:18:40 +0800

    DONE: Add some scripts to setup.

--- scripts/setup-dev.sh
+++ scripts/setup-dev.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env sh
 
-# Write some scripts in this place.
-
+if ! (docker run --name dectub.db -e POSTGRES_PASSWORD=1234 -p 22040:5432 -d postgres:11); then
+  echo "Failed to start database service"
+  exit 1
+fi
+sleep 1
+if ! (docker exec -it dectub.db createdb -U postgres -O postgres dectub); then
+  echo "Failed to start database service"
+  exit 1
+fi
+if ! (docker run --name dectub.redis -p 22050:6379 -d redis:6.2.5); then
+  echo "Failed to start database service"
+  exit 1
+fi
 if ! (./scripts/setup-git.sh); then
   echo "Failed to init git hooks"
   exit 1
