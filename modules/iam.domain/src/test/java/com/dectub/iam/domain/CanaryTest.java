889febb9b98028ededd3d5a06391480abd0f0b57
Neil Wang <wangweili457@gmail.com>
2021-09-18 14:51:00 +0800

    DONE: Add iam module.

---
+++ modules/iam.domain/src/test/java/com/dectub/iam/domain/CanaryTest.java
@@ -0,0 +1,17 @@
+package com.dectub.iam.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Created by Neil Wang
+ * @version 1.0.0
+ * @date 2021/9/18 2:49 下午
+ */
+public class CanaryTest {
+    @Test
+    void should_test_correctly() {
+        assertThat("ok").isNotBlank();
+    }
+}
---
+++ modules/iam.gateways/src/test/java/com/dectub/iam/gateways/CanaryTest.java
@@ -0,0 +1,17 @@
+package com.dectub.iam.gateways;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Created by Neil Wang
+ * @version 1.0.0
+ * @date 2021/9/18 2:47 下午
+ */
+public class CanaryTest {
+    @Test
+    void should_run_test_correctly() {
+        assertThat("ok").isNotBlank();
+    }
+}
