7526067e51238712344f523e5f2e52c06626953e
Neil Wang <wangweili457@gmail.com>
2021-12-01 14:32:03 +0800

    DONE: Fix bad smell

--- scripts/qodana-report.sh
+++ scripts/qodana-report.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
-my_dir=`pwd`
 docker run --rm -it -p 8080:8080 \
-  -v  $my_dir/:/data/project/ \
-  -v  $my_dir/build/qodana/:/data/results/ \
-jetbrains/qodana-jvm --show-report
+  -v $(pwd)/:/data/project/ \
+  -v $(pwd)/build/qodana/:/data/results/ \
+  jetbrains/qodana-jvm --show-report
